5a108e8b51da0902695e3aa7dae1210f91b82692
Sagar ghare <sagar.ghare@acc.ltd>
2024-02-18 16:55:25 +0530

    reusable componnent added loader and font

--- Component/Home/index.jsx
+++ Component/Home/index.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Text } from "react-native";
-import WebView from "react-native-webview";
+import Loader from "../Reusable/Loader";
+import { View } from "react-native";
+import Font from "../Reusable/Font";
 
 const Home = () => {
     return (
-        <WebView
-            source={{ uri: 'https://www.w3schools.com/' }}
-            style={{
-                // marginTop: 37
-            }}
-        />
+        <View>
+            <Font size="xs" style={{ color: 'red' }}>Just CODING</Font>
+            <Font size="sm">Just CODING</Font>
+            <Font size="md">Just CODING</Font>
+            <Font size="lg">Just CODING</Font>
+            <Font size="xl">Just CODING</Font>
+            <Font size="xxl">Just CODING</Font>
+            <Font style={{ color: 'blue' }}>Just CODING</Font>
+            <Loader
+                size={180}
+                color="red"
+                style={{
+                    margin: 24
+                }}
+            />
+        </View>
     );
 }
 
