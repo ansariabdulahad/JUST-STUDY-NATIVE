77bc21357e3fe85897ce0e4c20621c4cace358c0
Sagar ghare <sagar.ghare@acc.ltd>
2024-02-25 15:32:14 +0530

    Animated added prop: timing, start, parallel & sequence

--- Component/Login/index.jsx
+++ Component/Login/index.jsx
@@ -1,15 +1,81 @@
-import React from "react";
-import { Text } from "react-native";
-import WebView from "react-native-webview";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Button, View } from "react-native";
 
 const Login = () => {
+
+    const [finish, setFinish] = useState(false);
+    const height = useRef(new Animated.Value(20)).current; // useref use when you want to change varaibale as muted - means it not render again and again.
+    const width = useRef(new Animated.Value(20)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    const open = () => { // Animated.parallel & Animated.sequence
+
+        Animated
+            .sequence([
+                Animated
+                    .timing(height, {
+                        toValue: 300,
+                        duration: 1000,
+                        useNativeDriver: false
+                    }),
+                Animated
+                    .timing(width, {
+                        toValue: 300,
+                        duration: 1000,
+                        useNativeDriver: false
+                    }),
+                // Animated
+                //     .timing(opacity, {
+                //         toValue: 1,
+                //         duration: 1000,
+                //         useNativeDriver: false
+                //     })
+            ])
+            .start(() => setFinish(true));
+    }
+
+    const close = () => {
+
+        Animated
+            .sequence([
+                Animated
+                    .timing(height, {
+                        toValue: 20,
+                        duration: 1000,
+                        useNativeDriver: false
+                    }),
+                Animated
+                    .timing(width, {
+                        toValue: 20,
+                        duration: 1000,
+                        useNativeDriver: false
+                    }),
+                // Animated
+                //     .timing(opacity, {
+                //         toValue: 0,
+                //         duration: 1000,
+                //         useNativeDriver: false
+                //     })
+            ])
+            .start(() => setFinish(false));
+    }
+
     return (
-        <WebView
-            source={{ uri: 'https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com' }}
-            style={{
-                // marginTop: 37
-            }}
-        />
+        <View>
+            <Button
+                title="Animate Now"
+                onPress={finish ? close : open}
+            />
+            <Animated.View
+                style={{
+                    width: width,
+                    height: height,
+                    backgroundColor: 'tomato',
+                    margin: 20,
+                    // opacity: opacity
+                }}
+            />
+        </View>
     );
 }
 
