ece3d4b89bc292eb14d23d9092527a8279b84595
Sagar ghare <sagar.ghare@acc.ltd>
2024-02-27 21:51:20 +0530

    interpolate method used in width, height and backgroundcolor in style

--- Component/Login/index.jsx
+++ Component/Login/index.jsx
@@ -1,46 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Button, View } from "react-native";
+import { Animated, Button, Text, View } from "react-native";
 
 const Login = () => {
 
-    const fade = useRef(new Animated.Value(0)).current;
+    const anim = useRef(new Animated.Value(0)).current;
 
     useEffect(() => {
-        Animated
-            .loop(
-                Animated
-                    .sequence([
-                        Animated
-                            .timing(fade, {
-                                toValue: 1,
-                                duration: 500,
-                                useNativeDriver: true
-                            }),
-                        Animated
-                            .delay(1000),
-                        Animated
-                            .timing(fade, {
-                                toValue: 0,
-                                duration: 500,
-                                useNativeDriver: true
-                            })
-                    ]),
-                {
-                    iterations: 10,
-                }
-            )
-            .start()
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(anim, {
+                    toValue: 1,
+                    useNativeDriver: false
+                }),
+                Animated.timing(anim, {
+                    toValue: 2,
+                    useNativeDriver: false
+                }),
+                Animated.timing(anim, {
+                    toValue: 3,
+                    useNativeDriver: false
+                }),
+                Animated.timing(anim, {
+                    toValue: 4,
+                    useNativeDriver: false
+                })
+            ]),
+            {
+                iterations: 5,
+                useNativeDriver: true
+            }
+        ).start();
     }, []);
 
     return (
         <View>
             <Animated.View
                 style={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: 'blue',
+                    width: anim.interpolate({
+                        inputRange: [0, 1, 2, 3, 4],
+                        outputRange: [20, 40, 60, 80, 100]
+                    }),
+                    height: anim.interpolate({
+                        inputRange: [0, 1, 2, 3, 4],
+                        outputRange: [20, 40, 60, 80, 100]
+                    }),
                     margin: 20,
-                    opacity: fade
+                    opacity: 1,
+                    backgroundColor: anim.interpolate({
+                        inputRange: [0, 1, 2, 3, 4],
+                        outputRange: ['red', 'green', 'blue', 'yellow', 'black'],
+                    })
                 }}
             />
         </View>
